fd43858f6f2f7dcc156c265485bf81c46be7770f
Tran Ho Nhi Thanh <thanh.tran@bnksolution.com>
2021-07-14 17:18:56 +0700

    add login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,20 +1,69 @@
-export const LoginPage = () => {
-    return (
-        <div>
-            <div className="flex justify-center">
-                <img className= "w-48 h-48" src="https://ouch-cdn2.icons8.com/8kbGG_blA2KmQZCqyZtiC3x4C-KL05sdZprGEOCg054/rs:fit:912:912/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNTEx/LzZhOGNlN2U4LWMz/ODEtNGVlZC05MDlm/LTkxNzVmY2YwMmIz/MS5zdmc.png" />
-            </div>
+import React, {useState} from 'react'
 
-            <div class="px-11 pb-7">
-                <p class="text-3xl font-bold tracking-wide pb-8">Login</p>
-                <div class="mb-5 py-2 border-b">
-                    <input placeholder="Username" />
-                </div>
-                <div class="py-2 mb-5 border-b">
-                    <input placeholder="Password" />
-                </div>
-                <div class="rounded-xl bg-yellow-300 tracking-wide p-3 text-base shadow-lg text-center">Login</div>
+export const LoginPage = () => {
+  const [email,
+    setEmail] = useState('')
+  const [password,
+    setPassword] = useState('')
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+      <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+        <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+          <div className="px-5 py-7">
+            <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="text"
+              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"/>
+            <label className="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="text"
+              class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"/>
+            <button
+              type="button"
+              class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
+              <span className="inline-block mr-2">Login</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="w-4 h-4 inline-block">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"/>
+              </svg>
+            </button>
+          </div>
+          <div className="py-5">
+            <div className="grid grid-cols-2 gap-1">
+              <div className="text-center sm:text-left whitespace-nowrap">
+                <button
+                  className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    class="w-4 h-4 inline-block align-text-top">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"/>
+                  </svg>
+                  <span className="inline-block ml-1">Forgot Password</span>
+                </button>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
